b5e1e7f26f72560551113d8b43b254407d31246c
b23409 <126991880+b23409@users.noreply.github.com>
2023-03-04 18:42:40 -0500

    Update chat.py
    index workaround

--- modules/chat.py
+++ modules/chat.py
@@ -59,7 +59,10 @@ def extract_message_from_reply(question, reply, current, other, check, extension
     print(previous_idx)
     idx = [m.start() for m in re.finditer(f"(^|\n){re.escape(current)}:", reply)]
     print(idx)
-    idx = idx[len(previous_idx)-1]
+    if len(idx) == 1:
+        idx = idx[0]
+    else:
+        idx = idx[len(previous_idx)-1]
 
     if extensions:
         reply = reply[idx + 1 + len(apply_extensions(f"{current}:", "bot_prefix")):]

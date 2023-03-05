24f6f29919f7823b4141f584ed3abb65eb7a4a0e
b23409 <126991880+b23409@users.noreply.github.com>
2023-03-04 19:10:22 -0500

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -57,6 +57,7 @@ def extract_message_from_reply(question, reply, current, other, check, extension
 
     previous_idx = [m.start() for m in re.finditer(f"(^|\n){re.escape(current)}:", question)]
     print(previous_idx)
+    print(reply)
     idx = [m.start() for m in re.finditer(f"(^|\n){re.escape(current)}:", reply)]
     print(idx)
     if len(idx) == 1:
